451ffa48d65f67d6c61047853af591f7c21b155d
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2025-07-27 15:57:53 -0300

    chore: organizando as pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './screens/Login';
-import Cadastro from './screens/Cadastro';
+import Login from './screens/Auth/Login';
+import Cadastro from './screens/Auth/Cadastro';
 import { AuthProvider } from './context/auth';
 import Home from './screens/Home';
 import { ProtectedRoute } from './components/Auth/ProtectedRoute';
 import SidebarRoutes from './components/Sidebar';
-import ModelosProjeto from './screens/ModelosProjeto';
-import CriarProjeto from './screens/CriarProjeto';
-import RealizarAporte from './screens/RealizarAporte';
-import AportesRealizados from './screens/AportesRealizados';
-import ValidarAporte from './screens/ValidarAporte';
-import AvaliarProjetos from './screens/AvaliarProjeto';
-import ProjetosAvaliados from './screens/ProjetosAvaliados';
-import ProjetosSalvos from './screens/ProjetosSalvos';
-import ProjetosSubmetidos from './screens/ProjetosSubmetidos';
-import EditarProjeto from './screens/EditarProjeto';
-import ProjetosAprovados from './screens/ProjetosAprovados';
-import ExecucaoMarco from './screens/ExecucaoMarco';
-import AnaliseMarco from './screens/AnaliseMarco';
-import AvaliacaoEvidencias from './screens/AvaliacaoEvidencias';
+import ModelosProjeto from './screens/EntExec/ModelosProjeto';
+import CriarProjeto from './screens/EntExec/CriarProjeto';
+import RealizarAporte from './screens/Investidor/RealizarAporte';
+import AportesRealizados from './screens/Investidor/AportesRealizados';
+import ValidarAporte from './screens/MembroCBH/ValidarAporte';
+import AvaliarProjetos from './screens/MembroCBH/AvaliarProjeto';
+import ProjetosAvaliados from './screens/MembroCBH/ProjetosAvaliados';
+import ProjetosSalvos from './screens/EntExec/ProjetosSalvos';
+import ProjetosSubmetidos from './screens/EntExec/ProjetosSubmetidos';
+import EditarProjeto from './screens/EntExec/EditarProjeto';
+import ProjetosAprovados from './screens/MembroCBH/ProjetosAprovados';
+import ExecucaoMarco from './screens/EntExec/ExecucaoMarco';
+import AnaliseMarco from './screens/MembroCBH/AnaliseMarco';
+import AvaliacaoEvidencias from './screens/MembroCBH/AvaliacaoEvidencias';
 
 function App() {
 
